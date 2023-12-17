c99bdcfe9a16d637a4e0c8befd3fe0eaca364ad8
bluksy <bluksy95@gmail.com>
2023-12-17 13:40:49 +0100

    change max length to env variable

--- twitch_to_youtube.sh
+++ twitch_to_youtube.sh
@@ -23,11 +23,13 @@ if [ -n "$TIMEZONE" ]; then
 fi
 
 _retry_time=${RETRY_TIME:-30s}
-_description=${DESCRIPTION:""}
+_description=${DESCRIPTION:-""}
+_max_length=${MAX_LENGTH:-"8:00:00"}
 
 log "Streamer name: $STREAMER_NAME"
 log "Retry time: $_retry_time"
 log "Description: $_description"
+log "Max length: $_max_length"
 
 while [ ! -f ./twitch_to_youtube.lock ]; do
   _stream_title=$(streamlink twitch.tv/"$STREAMER_NAME" -j | jq '.metadata?.title?' || true)
@@ -58,9 +60,8 @@ while [ ! -f ./twitch_to_youtube.lock ]; do
     "description": "%s"
   }' "${STREAMER_NAME}" "${_current_timedate}" "${_stream_title}" "${_current_timedate}" "${_description}" > ./yt_input
 
-  # Limit the stream duration to 10 hours for YouTube
   streamlink twitch.tv/$STREAMER_NAME best \
-    --hls-duration 10:00:00 \
+    --hls-duration $_max_length \
     --twitch-disable-hosting \
     --config ./auth/config.twitch \
     -O 2>/dev/null | ./youtubeuploader/youtubeuploader \
