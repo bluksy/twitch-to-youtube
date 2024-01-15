c9de83a35b2163edcd5d1f448dca102c7434cb99
bluksy <bluksy95@gmail.com>
2024-01-15 12:18:15 +0100

    fix update video, add option to keep the video private

--- update_latest_video.sh
+++ update_latest_video.sh
@@ -33,20 +33,9 @@ _latest_video_id=$(echo "$_latest_video_detail" | jq -r '.id')
 _latest_video_title=$(echo "$_latest_video_detail" | jq '.snippet.title')
 _latest_video_title=${_latest_video_title:1:-1}
 _latest_video_category=$(echo "$_latest_video_detail" | jq '.snippet.categoryId')
+_latest_video_category=${_latest_video_category:1:-1}
 
 log "youtubeuploader output: $_latest_video_detail"
-
-# Set the desired publish date and time in RFC3339 format
-# Get current timestamp
-_current_timestamp=$(date -u +%s)
-
-# Calculate timestamp for tomorrow
-_tomorrow_timestamp=$((_current_timestamp + PUBLISH_DELAY_SECONDS))
-
-# Convert tomorrow's timestamp to RFC3339 format
-_publish_time=$(date -u -d "@$_tomorrow_timestamp" +"%Y-%m-%dT%H:%M:%SZ")
-
-log "Publish time: $_publish_time"
 
 # if title_changes file doesn't exist then use just description from env variable
 if [ ! -f "./title_changes.$_recording_id" ];
@@ -57,18 +46,42 @@ else
   rm "./title_changes.$_recording_id"
 fi
 
-# Build the JSON data for the API request
-_update_video_request_body='{
-  "id": "'$_latest_video_id'",
-  "status": {
-    "publishAt": "'$_publish_time'",
-    "privacyStatus": "private"
-  },
-  "snippet": {
-    "title":"'$_latest_video_title'",
-    "description":"'$_description'",
-    "categoryId":"'$_latest_video_category'"
-  }
-}'
+if [ "$PUBLISH_DELAY_SECONDS" -lt 0 ]; then
+  # Build the JSON data for the API request
+  _update_video_request_body='{
+    "id": "'$_latest_video_id'",
+    "status": {
+      "privacyStatus": "private"
+    },
+    "snippet": {
+      "title":"'$_latest_video_title'",
+      "description":"'$_description'",
+      "categoryId":"'$_latest_video_category'"
+    }
+  }'
+  else
+    _current_timestamp=$(date -u +%s)
+
+    _delayed_timestamp=$((_current_timestamp + PUBLISH_DELAY_SECONDS))
+
+    # Convert timestamp to RFC3339 format
+    _publish_time=$(date -u -d "@$_delayed_timestamp" +"%Y-%m-%dT%H:%M:%SZ")
+
+    log "Publish time: $_publish_time"
+
+    # Build the JSON data for the API request
+    _update_video_request_body='{
+      "id": "'$_latest_video_id'",
+      "status": {
+        "publishAt": "'$_publish_time'",
+        "privacyStatus": "private"
+      },
+      "snippet": {
+        "title":"'$_latest_video_title'",
+        "description":"'$_description'",
+        "categoryId":"'$_latest_video_category'"
+      }
+    }'
+fi
 
 update_video "$_youtube_api_token" "$_update_video_request_body"
