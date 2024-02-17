f6d5a12262ef36f50e27f9d0b0f07d66acb701fe
bluksy <bluksy95@gmail.com>
2024-02-17 10:29:03 +0100

    fix line breaks, update var names

--- update_videos.sh
+++ update_videos.sh
@@ -31,22 +31,21 @@ while IFS='' read -r _recording_id || [ -n "${_recording_id}" ]; do
   # Get publish delay from env variables; default is 1 day
   PUBLISH_DELAY_SECONDS=${PUBLISH_DELAY_SECONDS:-86400}
 
-  # Get the latest video ID and title
-  _latest_video_detail=$(cat "./yt_output.$_recording_id")
-  _latest_video_id=$(echo "$_latest_video_detail" | jq -r '.id')
-  _latest_video_title=$(echo "$_latest_video_detail" | jq '.snippet.title')
-  _latest_video_title=${_latest_video_title:1:-1}
-  _latest_video_category=$(echo "$_latest_video_detail" | jq '.snippet.categoryId')
-  _latest_video_category=${_latest_video_category:1:-1}
-  log "youtubeuploader output: $_latest_video_detail" "$_recording_id"
+  _current_video_detail=$(cat "./yt_output.$_recording_id")
+  _current_video_id=$(echo "$_current_video_detail" | jq -r '.id')
+  _current_video_title=$(echo "$_current_video_detail" | jq '.snippet.title')
+  _current_video_title=${_current_video_title:1:-1}
+  _current_video_category=$(echo "$_current_video_detail" | jq '.snippet.categoryId')
+  _current_video_category=${_current_video_category:1:-1}
+  log "youtubeuploader output: $_current_video_detail" "$_recording_id"
 
   _part=1
   _description=""
 
   for _video_id in "$@"; do
-    if [[ "$_video_id" = "$_latest_video_id" ]]; then
-      if [[ ${#_latest_video_title} -lt 93 ]]; then
-        _latest_video_title=$(printf "%s part %s" "${_latest_video_title}" ${_part})
+    if [[ "$_video_id" = "$_current_video_id" ]]; then
+      if [[ ${#_current_video_title} -lt 93 ]]; then
+        _current_video_title=$(printf "%s part %s" "${_current_video_title}" ${_part})
       fi
 
       _part=$((_part + 1))
@@ -57,26 +56,28 @@ while IFS='' read -r _recording_id || [ -n "${_recording_id}" ]; do
     _part=$((_part + 1))
   done
 
+  _description=$(printf "%s\\n" "${_description}")
+
   # if title_changes file doesn't exist then use just description from env variable
   if [ ! -f "./title_changes.$_recording_id" ];
   then
-    _description=$(printf '\\n\\n%s\\n%s' "${_description}" "${DESCRIPTION}" )
+    _description=$(printf '%s\\n%s' "${_description}" "${DESCRIPTION}" )
   else
-    _description=$(printf '\\n\\n%s\\n%s\\n%s' "${_description}" "$(cat "./title_changes.$_recording_id")" "${DESCRIPTION}" )
+    _description=$(printf '%s\\n%s\\n%s' "${_description}" "$(cat "./title_changes.$_recording_id")" "${DESCRIPTION}" )
     rm "./title_changes.$_recording_id"
   fi
 
   if [ "$PUBLISH_DELAY_SECONDS" -lt 0 ]; then
     # Build the JSON data for the API request
     _update_video_request_body='{
-      "id": "'$_latest_video_id'",
+      "id": "'$_current_video_id'",
       "status": {
         "privacyStatus": "private"
       },
       "snippet": {
-        "title":"'$_latest_video_title'",
+        "title":"'$_current_video_title'",
         "description":"'$_description'",
-        "categoryId":"'$_latest_video_category'"
+        "categoryId":"'$_current_video_category'"
       }
     }'
     else
@@ -89,15 +90,15 @@ while IFS='' read -r _recording_id || [ -n "${_recording_id}" ]; do
 
       # Build the JSON data for the API request
       _update_video_request_body='{
-        "id": "'$_latest_video_id'",
+        "id": "'$_current_video_id'",
         "status": {
           "publishAt": "'$_publish_time'",
           "privacyStatus": "private"
         },
         "snippet": {
-          "title":"'$_latest_video_title'",
+          "title":"'$_current_video_title'",
           "description":"'$_description'",
-          "categoryId":"'$_latest_video_category'"
+          "categoryId":"'$_current_video_category'"
         }
       }'
   fi
