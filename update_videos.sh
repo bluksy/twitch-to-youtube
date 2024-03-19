265c8a2b021b23da19d8ae380ce833dc8b2f03a1
bluksy <bluksy95@gmail.com>
2024-03-19 08:46:59 +0100

    fix video parts

--- update_videos.sh
+++ update_videos.sh
@@ -52,26 +52,26 @@ while IFS='' read -r _recording_id || [ -n "${_recording_id}" ]; do
   for _video_id in "$@"; do
     _part=$((_part + 1))
 
-    _duration=10800
-
-    if [ -f "./recording_duration.$_recording_id" ]; then
-      _duration=$(cat "./recording_duration.$_recording_id")
-      rm "./recording_duration.$_recording_id"
-    fi
-
-    _patch_vod_request_body='{
-      "vodId": "'$_vod_id'",
-      "part": "'$_part'",
-      "youtubeId": "'$_current_video_id'",
-      "duration": '$_duration'
-    }'
-
-    if [[ "$_video_counter" -ne 1 ]] && [[ ${#_current_video_title} -lt 93 ]]; then
-      _current_video_title=$(printf "%s part %s" "${_current_video_title}" ${_part})
-    fi
-
     if [[ "$_video_id" != "$_current_video_id" ]]; then
       _description=$(printf "%sPART %s: https://www.youtube.com/watch?v=%s\\n\\n\\n" "${_description}" ${_part} "${_video_id}")
+    else
+      _duration=10800
+
+      if [ -f "./recording_duration.$_recording_id" ]; then
+        _duration=$(cat "./recording_duration.$_recording_id")
+        rm "./recording_duration.$_recording_id"
+      fi
+
+      _patch_vod_request_body='{
+        "vodId": "'$_vod_id'",
+        "part": "'$_part'",
+        "youtubeId": "'$_current_video_id'",
+        "duration": '$_duration'
+      }'
+
+      if [[ "$_video_counter" -ne 1 ]] && [[ ${#_current_video_title} -lt 93 ]]; then
+        _current_video_title=$(printf "%s part %s" "${_current_video_title}" ${_part})
+      fi
     fi
   done
 
